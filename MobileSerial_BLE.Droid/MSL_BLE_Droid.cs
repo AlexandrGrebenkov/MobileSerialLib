bf6364e45867198ebe7f6d8797c10734ccce1241
Alexandr <alexandrgrebenkov@gmail.com>
2018-02-28 16:20:21 +0500

    Переписан класс  MSL_BLE_Droid

--- MobileSerial_BLE.Droid/MSL_BLE_Droid.cs
+++ MobileSerial_BLE.Droid/MSL_BLE_Droid.cs
@@ -3,57 +3,30 @@ using AndroidBluetoothLE.Bluetooth.Client;
 using Java.Util;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading;
-using MobileSerial_BLE;
 using System.Threading.Tasks;
 
 namespace MobileSerial_BLE.Droid
 {
     public class MSL_BLE_Droid : IMSerial_BLE
     {
-        BluetoothConnectionHandler _connectionHandler;
         DeviceWritingHandler _writingHandler;
         BluetoothGattCharacteristic _characteristic;
         BluetoothDevice device;
 
-        BluetoothClient _bluetoothClient;
         BluetoothDeviceScanner _scanner;
-
-        ObservableCollection<BluetoothDevice> _deviceList = new ObservableCollection<BluetoothDevice>();
+        List<BluetoothDevice> DeviceList = new List<BluetoothDevice>();
 
         public BLE_Status Status { get; set; }
-
-        byte[] RxData;
-        //====================================
-        IEnumerable<BluetoothGattService> GetServices()
-        {
-            var filterUuid = new[] {
-                UUID.FromString("00001800-0000-1000-8000-00805F9B34FB"),
-                UUID.FromString("00001801-0000-1000-8000-00805F9B34FB"),
-                UUID.FromString("7905F431-B5CE-4E99-A40F-4B1E122D00D0") };
-            var serviceList = _connectionHandler.GetServiceList().Where(s => filterUuid.All(uuid => !uuid.Equals(s.Uuid)));
-            return serviceList;
-        }
-
-        BluetoothGattCharacteristic GetCharacteristic(IEnumerable<BluetoothGattService> serviceList)
-        {
-            var uuid = UUID.FromString("49535343-1e4d-4bd9-ba61-23c647249616");
-
-            var service = serviceList.First(s => s.Characteristics.Any(ch => ch.Uuid.Equals(uuid)));
-            return service.Characteristics.First(ch => ch.Uuid.Equals(uuid));
-        }
-        //====================================
 
         /// <summary>
         /// Инициализация Bluetooth-модуля
         /// </summary>
         /// <returns></returns>
         public BLE_Status BLE_Init()
-        {
-            _bluetoothClient = BluetoothClient.Instance;
+        {             
+            BluetoothClient _bluetoothClient = BluetoothClient.Instance;
             _bluetoothClient.Initialize();
 
             if (_bluetoothClient.Adapter == null) return BLE_Status.BT_NotAwailable;  //Bluetooth не поддерживается на этом устройстве
@@ -65,11 +38,11 @@ namespace MobileSerial_BLE.Droid
 
             _scanner = new BluetoothDeviceScanner(_bluetoothClient.Adapter, (BluetoothDevice dev) =>
             {
-                if (_deviceList.All(d => !d.Address.Equals(dev.Address, StringComparison.OrdinalIgnoreCase)))
+                if (DeviceList.All(d => !d.Address.Equals(dev.Address, StringComparison.OrdinalIgnoreCase)))
                 {
                     if (dev.Name != null)
                     {
-                        _deviceList.Add(dev);
+                        DeviceList.Add(dev);
                         onDeviceFound?.Invoke(new BLE_Device_Info() { Name = dev.Name, Address = dev.Address });
                     }
                 }
@@ -78,8 +51,16 @@ namespace MobileSerial_BLE.Droid
             return BLE_Status.NotConnect;
         }
 
+        #region Connect/Disconnect
+        BluetoothConnectionHandler _connectionHandler;
+
+        /// <summary>
+        /// Подключение к девайсу
+        /// </summary>
+        /// <param name="action"></param>
         public void Connect(Action<bool> action)
         {
+            //Callback hell!
             _connectionHandler = BluetoothClient.Instance.ConnectionHandler;
             if (_connectionHandler.IsConnected) return;
 
@@ -90,36 +71,33 @@ namespace MobileSerial_BLE.Droid
                 {
                     case ProfileState.Connected:
                     {//Подключились
-                            _connectionHandler.DiscoverServices(status =>
-                        {
-                            if (status == GattStatus.Success)
-                            {//Получен список сервисов
-                                    var serviceList = GetServices();
+                        _connectionHandler.DiscoverServices(status =>
+                    {
+                        if (status == GattStatus.Success)
+                        {//Получен список сервисов
+                            _characteristic = GetCharacteristic(GetServices());
+                            var notify = new DeviceNotifyingHandler(_connectionHandler.GattValue, GattClientObserver.Instance);
 
-                                _characteristic = GetCharacteristic(serviceList);
-                                var notify = new DeviceNotifyingHandler(_connectionHandler.GattValue, GattClientObserver.Instance);
+                            notify.Subscribe(_characteristic, (bool notifyStatus) =>
+                            {
+                                if (notifyStatus == true)
+                                {//Нотификация пройдена
+                                    _writingHandler = new DeviceWritingHandler(_connectionHandler.GattValue, GattClientObserver.Instance);
+                                    _writingHandler.ReceivedReadResponce += _writingHandler_ReceivedReadResponce;
+                                    Status = BLE_Status.Connect;
+                                    action?.Invoke(true);
+                                }
+                                else
+                                {//Если нотификация не пройдена, то делать нам тут нечего и мы отключаемся от девайса
+                                    action?.Invoke(false);
+                                }
+                                notify.Dispose();//при любом результате выходим так
+                            });
 
-                                notify.Subscribe(_characteristic, (bool notifyStatus) =>
-                                {
-                                    if (notifyStatus == true)
-                                    {//Нотификация пройдена
-                                            _writingHandler = new DeviceWritingHandler(_connectionHandler.GattValue, GattClientObserver.Instance);
-                                        _writingHandler.ReceivedReadResponce += _writingHandler_ReceivedReadResponce;
-                                        Status = BLE_Status.Connect;
-                                        action?.Invoke(true);
-                                    }
-                                    else
-                                    {
-                                            //_writingHandler.ReceivedReadResponce -= _writingHandler_ReceivedReadResponce;
-                                            action?.Invoke(false);
-                                    }
-                                    notify.Dispose();
-                                });
-
-                            }
-                            else
-                                action?.Invoke(false);
-                        });
+                        }
+                        else
+                            action?.Invoke(false);
+                    });
                         break;
                     }
                     case ProfileState.Disconnected:
@@ -136,12 +114,45 @@ namespace MobileSerial_BLE.Droid
                 }
             });
 
+
+            //====================================
+            IEnumerable<BluetoothGattService> GetServices()
+            {
+                var filterUuid = new[] {
+                UUID.FromString("00001800-0000-1000-8000-00805F9B34FB"),
+                UUID.FromString("00001801-0000-1000-8000-00805F9B34FB"),
+                UUID.FromString("7905F431-B5CE-4E99-A40F-4B1E122D00D0") };
+                return _connectionHandler?.GetServiceList().Where(s => filterUuid.All(uuid => !uuid.Equals(s.Uuid)));
+            }
+
+            BluetoothGattCharacteristic GetCharacteristic(IEnumerable<BluetoothGattService> serviceList)
+            {
+                var uuid = UUID.FromString("49535343-1e4d-4bd9-ba61-23c647249616");
+
+                var service = serviceList.First(s => s.Characteristics.Any(ch => ch.Uuid.Equals(uuid)));
+                return service.Characteristics.First(ch => ch.Uuid.Equals(uuid));
+            }
+            //====================================
         }
 
+        /// <summary>
+        /// Отключение
+        /// </summary>
         public void Disconnect()
         {
             if (_connectionHandler.IsConnected == true)
                 _connectionHandler.DisconnectAsync();
+        }
+        #endregion
+
+        #region Запись/Чтение
+        /// <summary>Буфер приёма</summary>
+        byte[] RxData;
+
+        public void Write(byte[] TxBuff, int timeout = 1000)
+        {
+            _writingHandler.Write(TxBuff, _characteristic, true);
+            RxData = null;
         }
 
         /// <summary>
@@ -150,14 +161,9 @@ namespace MobileSerial_BLE.Droid
         /// <param name="data"></param>
         private void _writingHandler_ReceivedReadResponce(byte[] data)
         {
-            Debug.WriteLine("Thread name is: {0}.", Thread.CurrentThread.Name);
             RxData = data;
-            _execute?.BeginInvoke(data, (d) =>
-            {
-
-            }, null);//  Invoke(data);
+            _execute?.Invoke(data);
         }
-
 
         Action<byte[]> _execute;
 
@@ -167,7 +173,7 @@ namespace MobileSerial_BLE.Droid
         }
 
         /// <summary>
-        /// Метод чтения
+        /// Метод чтения (Блокирует UI)
         /// </summary>
         /// <param name="timeout">Таймаут (в мс)</param>
         /// <returns>Возвращает принятый буфер</returns>
@@ -195,6 +201,11 @@ namespace MobileSerial_BLE.Droid
             return data;
         }
 
+        /// <summary>
+        /// Асинхронный метод чтения
+        /// </summary>
+        /// <param name="timeout">Таймаут (в мс)</param>
+        /// <returns>Возвращает принятый буфер</returns>
         public async Task<byte[]> ReadAsync(uint timeout = 1000)
         {
             int t = 0;
@@ -218,13 +229,9 @@ namespace MobileSerial_BLE.Droid
             }
             return data;
         }
+        #endregion
 
-        public void Write(byte[] TxBuff, int timeout = 1000)
-        {
-            _writingHandler.Write(TxBuff, _characteristic, true);
-            RxData = null;
-        }
-
+        #region Сканирование/Выбор
         private Action<BLE_Device_Info> onDeviceFound;
         public void StartScan(Action<BLE_Device_Info> execute)
         {
@@ -233,51 +240,41 @@ namespace MobileSerial_BLE.Droid
                 (Status != BLE_Status.BLE_NotAwailable))
             {
                 onDeviceFound = execute;
-                _deviceList.Clear();
-                _scanner.StartScan();
+                DeviceList.Clear();//Очистка списка устройств
+                _scanner?.StartScan();//Запуск поиска
             }
         }
 
-        public void StopScan()
-        {
-            if ((Status != BLE_Status.BT_NotAwailable) &&
-                (Status != BLE_Status.BT_IsSwitchOff) &&
-                (Status != BLE_Status.BLE_NotAwailable))
-            {
-                _scanner.StopScan();
-            }
-        }
+        public void StopScan() => _scanner?.StopScan();
 
+        /// <summary>
+        /// Выбор устройства по его имени
+        /// </summary>
+        /// <param name="address">Имя устройства</param>
         public void SelectDeviceByName(string name)
         {
-            for (int i = 0; i < _deviceList.Count; i++)
-            {
-                if (_deviceList[i].Name == name)
-                {
-                    device = _deviceList[i];
-                    break;
-                }
-            }
+            device = DeviceList.FirstOrDefault(d => d.Name == name);
         }
 
+        /// <summary>
+        /// Выбор устройства по его MAC-адресу
+        /// </summary>
+        /// <param name="address">MAC-Адрес</param>
         public void SelectDeviceByAddress(string address)
         {
-            for (int i = 0; i < _deviceList.Count; i++)
-            {
-                if (_deviceList[i].Address == address)
-                {
-                    device = _deviceList[i];
-                    return;
-                }
-            }
+            //Сначала ищем прибор в списке найденых
+            device = DeviceList.FirstOrDefault(d => d.Address == address);
+            if (device != null) return;
 
+            //Если не нашли, то создаём новый BluetoothDevice по MAC-адресу
             if (BluetoothAdapter.CheckBluetoothAddress(address))
             {
-                BluetoothAdapter adapter = BluetoothAdapter.DefaultAdapter;
-                device = adapter.GetRemoteDevice(address);
+                device = BluetoothAdapter.DefaultAdapter.GetRemoteDevice(address);
+                DeviceList.Add(device);
             }
+            else
+                throw new ArgumentException("MAC-Адрес не валидный", nameof(address));
         }
-
-       
+        #endregion
     }
 }
