4cd3c503e37724413aea98fead43743ee8e71325
Alexandr <alexandrgrebenkov@gmail.com>
2018-03-01 14:23:07 +0500

    Начало работы над RFCOMM

--- MobileSerialLib/BLE/BLE_Device_Info.cs
+++ MobileSerialLib/BLE/BLE_Device_Info.cs
@@ -1,5 +1,8 @@
 ﻿namespace MobileSerialLib.BLE
 {
+    /// <summary>
+    /// Описание BLE-устройства
+    /// </summary>
     public class BLE_Device_Info
     {
         /// <summary>Имя устройства</summary>
--- MobileSerialLib/BLE/BLE_Status.cs
+++ MobileSerialLib/BLE/BLE_Status.cs
@@ -1,5 +1,8 @@
 ﻿namespace MobileSerialLib.BLE
 {
+    /// <summary>
+    /// Статус BLE-переферии
+    /// </summary>
     public enum BLE_Status
     {
         NotInit,//Не инициализирован ещё
